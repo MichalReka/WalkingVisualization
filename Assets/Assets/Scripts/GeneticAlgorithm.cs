2e0132c8a7c5016474924cac19cfd3d7c4b25738
Michał <michajoba@gmail.com>
2020-09-16 17:37:15 +0200

    database established, converting data to pdf, menu WIP

--- Assets/Assets/Scripts/GeneticAlgorithm.cs
+++ Assets/Assets/Scripts/GeneticAlgorithm.cs
@@ -6,72 +6,69 @@ using UnityEngine;
 
 public class GeneticAlgorithm
 {
-    private List<AnimalMovement> currentGeneration;   //list of animals
-    private AnimalBrain[] populationGenPool;
-    private List<AnimalBrain> parentsList;
-    private float mutationRate;
+    private List<AnimalMovement> _currentGeneration;   //list of animals
+    private AnimalBrain[] _populationGenPool;
+    private float _mutationRate;
     private int numberOfElites;
     public float bestDistance { get; private set; }
+    public float bestFitness { get; private set; }
     private float elitionismPercent = 0.05f;
+    List<float> distancesList;
     List<float> fitnessList;
     List<int> elitesIndexes;
     const int minFitness = -999;
-    float penaltyForCrash=1;
-    public GeneticAlgorithm(List<AnimalMovement> gen, float mutationRate)
+    // float penaltyForCrash = 1;
+    public GeneticAlgorithm(List<AnimalMovement> gen, float _mutationRate)
     {
-        currentGeneration = gen;
-        this.mutationRate = mutationRate;
-        populationGenPool = new AnimalBrain[currentGeneration.Count()];
-        numberOfElites = (int)Mathf.Ceil(currentGeneration.Count() * elitionismPercent);
+        _currentGeneration = gen;
+        this._mutationRate = _mutationRate;
+        _populationGenPool = new AnimalBrain[_currentGeneration.Count()];
+        numberOfElites = (int)Mathf.Ceil(_currentGeneration.Count() * elitionismPercent);
         AlgorithmStart();
-    }
-    float calculateFitness(AnimalMovement individual)
-    {
-        // var individualBody = individual.transform.Find("body").gameObject;
-        // sprawdzam wszystkie czesci ciala, dziele przez ilosc czesci ciala, tak otrzymuje jak daleko doszly i fitness (koniec z wyrzucaniem body d przodu)
-        int bodyPartsCount = individual.transform.childCount;
-        float fitness = 0;
-        for (int i = 0; i < bodyPartsCount; i++)
-        {
-            Transform child = individual.transform.GetChild(i);
-            fitness=fitness+child.transform.position.x;
-        }
-        fitness=fitness/bodyPartsCount;
-        fitness=fitness*individual.averageBodyY;
-        fitness=fitness+individual.timeBeingAlive;
-        if(individual.ifCrashed==true)
-        {
-            fitness=fitness*penaltyForCrash;   //jesli upadnie, nieznacznie zmniejszam fitness
-        }
-        return fitness;
-    }
-    public List<HingeArmPart> OrderAnimalChildren(GameObject obj)
-    {
-        HingeArmPart[] temp = obj.GetComponentsInChildren<HingeArmPart>();
-        HingeArmPart[] orderedHingeParts = new HingeArmPart[temp.Length];
-        int index = 0;
-        int noOfChildren = obj.transform.childCount;
-        for (int i = 0; i < noOfChildren; i++)
-        {
-            HingeArmPart childComponent = obj.transform.GetChild(i).GetComponent<HingeArmPart>();
-            if (childComponent != null)
-            {
-                orderedHingeParts[index] = childComponent;
-                index++;
-            }
-        }
-        return orderedHingeParts.ToList();
     }
     public List<AnimalBrain> GetPopulationGenPool()
     {
-        return populationGenPool.ToList();
+        return _populationGenPool.ToList();
+    }
+    float calculateFitness(AnimalMovement individual,float distance)
+    {
+        // var individualBody = individual.transform.Find("body").gameObject;
+        // sprawdzam wszystkie czesci ciala, dziele przez ilosc czesci ciala, tak otrzymuje jak daleko doszly i fitness (koniec z wyrzucaniem body d przodu)
+        float fitness = distance;
+        fitness = fitness * individual.averageBodyY;
+        fitness = fitness + individual.timeBeingAlive;
+        // if (individual.ifCrashed == true)
+        // {
+        //     fitness = fitness * penaltyForCrash;   //jesli upadnie, nieznacznie zmniejszam fitness
+        // }
+        return fitness;
+    }
+    float calculateDistance(AnimalMovement individual)
+    {
+        int bodyPartsCount = individual.transform.childCount;
+        float distance = 0;
+        for (int i = 0; i < bodyPartsCount; i++)
+        {
+            Transform child = individual.transform.GetChild(i);
+            distance = distance + child.transform.position.x;
+        }
+        distance = distance / bodyPartsCount;
+        return distance;
+    }
+    private void SetDistancesList()
+    {
+        distancesList = new List<float>();
+        for (int i = 0; i < _currentGeneration.Count; i++)
+        {
+            distancesList.Add(calculateDistance(_currentGeneration[i])); //tutaj bede trzymac wagi
+        }
     }
     private void SetFitnessList()
     {
         fitnessList = new List<float>();
-        for (int i = 0; i < currentGeneration.Count; i++)
+        for (int i = 0; i < _currentGeneration.Count; i++)
         {
-            fitnessList.Add(calculateFitness(currentGeneration[i])); //tutaj bede trzymac wagi
+            fitnessList.Add(calculateFitness(_currentGeneration[i],distancesList[i])); //tutaj bede trzymac wagi
         }
     }
     private void SetElitiesIndexesList()
@@ -87,39 +84,34 @@ public class GeneticAlgorithm
     }
     private void AlgorithmStart()
     {
+        SetDistancesList();
         SetFitnessList();
         SetElitiesIndexesList();
-        bestDistance = fitnessList.Max();
-        var bestDistanceIndex = fitnessList.IndexOf(bestDistance);
-        if(currentGeneration[bestDistanceIndex].ifCrashed)      //wlasciwe pokazywanie w GUI
-        {
-            bestDistance=bestDistance/penaltyForCrash;
-        }
-        bestDistance=bestDistance-currentGeneration[bestDistanceIndex].timeBeingAlive;
-        
-        bestDistance=bestDistance/currentGeneration[bestDistanceIndex].averageBodyY;    //wlasciwe pokazywanie w GUI
-        for (int i = 0; i < currentGeneration.Count; i++)
+        bestDistance = distancesList.Max();
+        bestFitness = fitnessList.Max();
+        var bestFitnessIndex = fitnessList.IndexOf(bestFitness);
+        for (int i = 0; i < _currentGeneration.Count; i++)
         {
             if (elitesIndexes.Contains(i))    //najlepszy zostaje
             {
-                populationGenPool[i] = new AnimalBrain();
-                populationGenPool[i].deepCopy(currentGeneration[i].animalBrain);
+                _populationGenPool[i] = new AnimalBrain();
+                _populationGenPool[i].deepCopy(_currentGeneration[i].animalBrain);
             }
             else
             {
-                populationGenPool[i] = Mate();
+                _populationGenPool[i] = Mate();
             }
         }
     }
     private int ChooseParent()
     {
-        int parentIndex = Random.Range(0, currentGeneration.Count);
+        int parentIndex = Random.Range(0, _currentGeneration.Count);
         float worstFitness = fitnessList.Min();
         float bestFitness = fitnessList.Max();
         float randomFitnessNumber = Random.Range(worstFitness, bestFitness);
         while (fitnessList[parentIndex] < randomFitnessNumber)
         {
-            parentIndex = Random.Range(0, currentGeneration.Count);
+            parentIndex = Random.Range(0, _currentGeneration.Count);
         }
         return parentIndex;
     }
@@ -134,11 +126,11 @@ public class GeneticAlgorithm
         int parent2Index = ChooseParent();
         AnimalBrain child = new AnimalBrain();
         float mixChance;
-        child.deepCopy(currentGeneration[parent1Index].animalBrain);
+        child.deepCopy(_currentGeneration[parent1Index].animalBrain);
         mixChance = Random.Range(0.0f, 100.0f);
-        child.mixWeights(currentGeneration[parent2Index].animalBrain, mixChance);
+        child.mixWeights(_currentGeneration[parent2Index].animalBrain, mixChance);
         float chance = Random.Range(0.0f, 1.0f);
-        if (chance < mutationRate) //obsluga mutacji - mutacja obejmuje zmiane indexu genu z losowym innym genem
+        if (chance < _mutationRate) //obsluga mutacji - mutacja obejmuje zmiane indexu genu z losowym innym genem
         {
             child.mutateWeights();
         }
